9f36aafaa62ff7ce988b95c665a1e18b13a0a5a8
Punith K <kpunith8@gmail.com>
2017-07-27 21:48:39 +0530

    Adding source map

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -2,6 +2,7 @@ import path from 'path';
 
   module.exports = {
     entry: './src/new-index.js',
+    devtool: 'inline-source-map',
     output: {
       filename: 'bundle.js',
       path: path.resolve(__dirname, 'src')
